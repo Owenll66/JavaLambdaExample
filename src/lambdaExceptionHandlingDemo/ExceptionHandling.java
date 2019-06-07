bc31b38fb343450c3ea54345e2c17ef501a4dbf5
bowen <592623044@qq.com>
2019-06-07 12:08:18 +0930

    update

--- src/lambdaExceptionHandlingDemo/ExceptionHandling.java
+++ src/lambdaExceptionHandlingDemo/ExceptionHandling.java
@@ -1,5 +1,15 @@
 package lambdaExceptionHandlingDemo;
 
 public class ExceptionHandling {
+	public static void main(String[] args) 
+	{
+		Example example = new Example();
+		
+		example.showLambdaExceptionHandlingExample();
+		System.out.println();
+		
+		
+	}
 
+	
 }
